e44aa9cac78db8a8c2aac06896df46094d85063f
DANKEBIBI <1580074116@qq.com>
2024-03-04 18:17:25 +0800

    -【测试环境】

---
+++ src/views/test/table/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { dkTable } from "dk-vue-components";
+import { onMounted } from "vue";
+import config from './config'
+onMounted(() => {});
+</script>
+<template>
+	<div class="list am_">
+		<dkTable :table-options="config" />
+	</div>
+</template>
+<style lang="scss" scoped>
+.list {
+	width: inherit;
+	height: inherit;
+    box-sizing: border-box;
+}
+</style>
